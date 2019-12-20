23f559cd1d262ad0a6fe0b4fea176065e2ccec0d
Dinh Thanh Thinh <thanhthinh95@gmail.com>
2019-12-20 23:14:43 +0700

    add comment in project

--- app/src/main/java/com/intel/smartlockers/Adapter/LockerAdapter.java
+++ app/src/main/java/com/intel/smartlockers/Adapter/LockerAdapter.java
@@ -79,27 +79,31 @@ public class LockerAdapter extends RecyclerView.Adapter<LockerAdapter.MyViewHold
                 ((Button) view.findViewById(R.id.btn_info_looker_done)).setText("Nhận tủ");
                 final EditText edit_data = view.findViewById(R.id.edit_info_locker_data);
 
+                //Khi click vào nút Trở lại
                 view.findViewById(R.id.btn_info_locker_back).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(final View view) {
-                        MainActivity.isOpenLooker = false;
+                        MainActivity.isOpenLooker = false;//Khóa người dùng không cho phép mở những tủ khác
                         alertDialog.dismiss();
                     }
                 });
 
+                //Khi click vào nút Nhận tủ
                 view.findViewById(R.id.btn_info_looker_done).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(final View view) {
                         lockers.setStatus(1);
                         lockers.setData(edit_data.getText().toString());
 
-                         notifyDataSetChanged();
+                        notifyDataSetChanged();//Cập nhật lại giao diện
 
-                        MainActivity.isOpenLooker = false;
+                        MainActivity.isOpenLooker = false;//Khóa người dùng không cho phép mở những tủ khác
+                        //Tạo mới lịch sử
                         MainActivity.baseSQLite.createHistory(new History(1,
                                 lockers.getID(), MainActivity.employeeIndex.getID(),
                                 new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault()).format(new Date()), 0));
 
+                        //Cập nhật lại trạng thái tủ
                         if(MainActivity.baseSQLite.updateLocker(lockers)){
                             Toast.makeText(view.getContext(), "Thành công", Toast.LENGTH_SHORT).show();
                         }else {
--- app/src/main/java/com/intel/smartlockers/MainActivity.java
+++ app/src/main/java/com/intel/smartlockers/MainActivity.java
@@ -81,30 +81,30 @@ public class MainActivity extends AppCompatActivity{
         if(event.getAction() == KeyEvent.ACTION_UP){
             if(event.getKeyCode() != KeyEvent.KEYCODE_ENTER){
                 codeRFID += (char) event.getUnicodeChar();
-            } else{
-                employeeIndex =  baseSQLite.getEmployee(codeRFID);
-                if(employeeIndex != null){
+            } else{//Đã đọc xong mã codeRFID
+
+                employeeIndex =  baseSQLite.getEmployee(codeRFID);//Lấy thông tin employee theo mã codeRFID
+                if(employeeIndex != null){//Kết quả trả về != null => Có employee này trong DB
                     Toast.makeText(this, employeeIndex.getName() + " - " + employeeIndex.getCode(), Toast.LENGTH_LONG).show();
 
-                    ArrayList<History> histories = baseSQLite.getHistoryForEmployee(employeeIndex.getID());
+                    ArrayList<History> histories = baseSQLite.getHistoryForEmployee(employeeIndex.getID());//Lấy lịch sử  truy cập hệ thống theo employee
                     Log.w("TAG_HIS", histories.toString() + "");
 
-
-                    if(histories.size() != 0){
-                        Lockers lockers = baseSQLite.getLocker(histories.get(0).getLockerID());
-                        if(lockers != null && lockers.getStatus() == 1){
-                            openLocker(lockers);
-                        }else{
-                            isOpenLooker = true;
+                    if(histories.size() != 0){//Có lịch sử trả về
+                        Lockers lockers = baseSQLite.getLocker(histories.get(0).getLockerID());//Lấy thông tin tủ theo lịch sử gần nhất
+                        if(lockers != null && lockers.getStatus() == 1){//Có thông tin tủ trả về và trạng thái đang là có người sử dụng. Tức là employee đang sử dụng tủ này
+                            openLocker(lockers);//Thực hiện mở tủ trên giao diện main
+                        }else{//Không có thông tin tủ trả về, hoặc trạng thái của tủ trả về là trống (chưa có ai sử dụng)
+                            isOpenLooker = true;//Cho phép người dùng click vào để mở tủ
                             Toast.makeText(this, "Hãy mở 1 tủ trống bất kỳ để đựng đồ", Toast.LENGTH_LONG).show();
-                            changeFragment(new fragmentHome(baseSQLite));
+                            changeFragment(new fragmentHome(baseSQLite));//Làm mới lại giao diện
                         }
-                    }else {
-                        isOpenLooker = true;
+                    }else {//Không có lịch sử trả về
+                        isOpenLooker = true;//Cho phép người dùng click vào để mở tủ
                         Toast.makeText(this, "Hãy mở 1 tủ trống bất kỳ để đựng đồ", Toast.LENGTH_LONG).show();
-                        changeFragment(new fragmentHome(baseSQLite));
+                        changeFragment(new fragmentHome(baseSQLite));//Làm mới lại giao diện
                     }
-                }else{
+                }else{//Thẻ này không có employee nào sở hữu
                     Toast.makeText(this, "Thẻ của bạn chưa được đăng ký", Toast.LENGTH_LONG).show();
                 }
 
@@ -134,19 +134,20 @@ public class MainActivity extends AppCompatActivity{
 
         final EditText edit_data = view.findViewById(R.id.edit_info_locker_data);
 
-
+        //Khi click vào nút Trả tủ
         view.findViewById(R.id.btn_info_locker_back).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
                 MainActivity.isOpenLooker = false;
                 lockers.setStatus(0);
-                if(baseSQLite.updateLocker(lockers)){
+                if(baseSQLite.updateLocker(lockers)){//Cập nhật lại trạng thái của tủ về 0
+                    //Tạo mới lịch sử
                     baseSQLite.createHistory(new History(1,
                             lockers.getID(), MainActivity.employeeIndex.getID(),
                             new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault()).format(new Date()), 1));
 
                     Toast.makeText(view.getContext(), "Trả tủ thành công", Toast.LENGTH_LONG).show();
-                    changeFragment(new fragmentHome(baseSQLite));
+                    changeFragment(new fragmentHome(baseSQLite));//Làm mới giao diện
                 }else {
                     Toast.makeText(view.getContext(), "Trả tủ thất bại", Toast.LENGTH_LONG).show();
                 }
@@ -155,17 +156,19 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+        //Khi click vào nút Đóng tủ
         view.findViewById(R.id.btn_info_looker_done).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
-                MainActivity.isOpenLooker = false;
+                MainActivity.isOpenLooker = false;//Khóa người dùng không cho phép mở những tủ khác
 
+                //Ghi lại lịch sử
                 baseSQLite.createHistory(new History(1,
                         lockers.getID(), MainActivity.employeeIndex.getID(),
                         new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault()).format(new Date()), 2));
 
                 lockers.setData(edit_data.getText().toString());
-                baseSQLite.updateLocker(lockers);
+                baseSQLite.updateLocker(lockers);//Cập nhật lại trạng thái tủ
                 alertDialog.dismiss();
             }
         });
